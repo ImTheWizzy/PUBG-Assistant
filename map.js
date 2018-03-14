79ec8ce6cebedac70ae371724bb81bf25a231f91
ImTheWizzy <martin_todo9@abv.bg>
2018-03-14 21:14:04 +0200

    Add files via upload

--- map.js
+++ map.js
@@ -5,3 +5,19 @@ function drawLoot() {
 function drawDrop() {
 	alert("Drop toggled!/CHECKING IF THIS WORKS");
 }
+
+function drawHeatmapErangel() {
+	document.getElementById("image").src = "erangel-heatmap.jpg";
+}
+
+function drawHeatmapMiramar() {
+	document.getElementById("image").src = "miramar-heatmap.jpg";
+}
+
+function resetMiramar() {
+	document.getElementById("image").src = "miramar.jpg";
+}
+
+function resetErangel() {
+	document.getElementById("image").src = "erangel.jpg";
+}
