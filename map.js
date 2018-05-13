b550b8c147fd8bd3f542431584fb87543cfd93b4
ImTheWizzy <martin_todo9@abv.bg>
2018-05-13 20:41:58 +0300

    Ah, much better...

---
+++ map.js
@@ -0,0 +1,23 @@
+function drawLootErangel() {
+	document.getElementById("map").src = "erangelLootmap.jpg";
+}
+
+function drawLootMiramar() {
+	document.getElementById("map").src = "miramarLootmap.jpg";
+}
+
+function drawHeatmapErangel() {
+	document.getElementById("map").src = "erangelHeatmap.jpg";
+}
+
+function drawHeatmapMiramar() {
+	document.getElementById("map").src = "miramarHeatmap.jpg";
+}
+
+function resetErangel() {
+	document.getElementById("map").src = "erangelMap.jpg";
+}
+
+function resetMiramar() {
+	document.getElementById("map").src = "miramarMap.jpg";
+}
